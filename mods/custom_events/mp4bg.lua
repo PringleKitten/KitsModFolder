19c0b5b132ca45b5b853c78979d86918f969624a
PringleKitten <wie054024@gmail.com>
2023-02-11 23:04:26 -0600

    Old Update

--- mods/custom_events/mp4bg.lua
+++
@@ -1,5 +0,0 @@
-function onEvent(name, value1, value2)
-    if name == "mp4bg" then
-        callScript('scripts/videoSprite', 'makeVideoSprite', {'video1', value1, -320, -180, 'camHUD'});
-    end
-end
