d4d6649f4f714947d48ce0e2fd027eb4b417477a
PringleKitten <125121975+PringleKitten@users.noreply.github.com>
2023-07-14 19:24:54 -0500

    assrfrfg

--- mods/data/youll-be-gone/script.lua
+++
@@ -1,7 +0,0 @@
-function onSongStart()
-    if difficulty == 1 then
-        setProperty('showComboNum', false)
-        setProperty('showRating', false)
-        callScript('scripts/videoSprite', 'makeVideoSprite', {'video29', 'ykg', -320, -180, 'camHUD',1,1})
-    end
-end
