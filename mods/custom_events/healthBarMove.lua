fc1da09d543bb46f0d644223fa23c1986bcaca96
PringleKitten <125121975+PringleKitten@users.noreply.github.com>
2023-09-09 13:22:38 -0500

    2.20.1
    slight event fixes for Healthbarmove.lua and newarrowtoggler.lua

--- mods/custom_events/healthBarMove.lua
+++ mods/custom_events/healthBarMove.lua
@@ -1,14 +1,13 @@
-
 function onEvent(name, value1, value2)
     if name == 'healthBarMove' then
         dX = getProperty('healthBar.x')
         dY = getProperty('healthBar.y')
         if value1 == 'left' then
-            value1 = screenWidth/10
+            value1 = screenWidth/-5
         elseif value1 == 'center' then
             value1 = dX
         elseif value1 == 'right' then
-            value1 = screenWidth/1.39
+            value1 = screenWidth/1.38
         end
 
         doTweenX('xBar', 'healthBar', value1, 0.2, 'circInOut')
