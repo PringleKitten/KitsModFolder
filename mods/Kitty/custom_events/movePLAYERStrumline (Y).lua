81851a205db388cc56c3e8962e9587b68292a859
PringleKitten <wie054024@gmail.com>
2023-11-28 22:06:29 -0600

    2.20.2.3
    fix for the new events. oopsie

--- mods/Kitty/custom_events/movePLAYERStrumline (Y).lua
+++ mods/Kitty/custom_events/movePLAYERStrumline (Y).lua
@@ -1,27 +1,21 @@
+function onSongStart()
+    dpsy0 = getPropertyFromGroup('playerStrums', 0, 'y')
+    dpsy1 = getPropertyFromGroup('playerStrums', 1, 'y')
+    dpsy2 = getPropertyFromGroup('playerStrums', 2, 'y')
+    dpsy3 = getPropertyFromGroup('playerStrums', 3, 'y')
+end
 function onEvent(name, value1, value2)
     if name == 'movePLAYERStrumline (Y)' then
         if getPropertyFromClass('ClientPrefs', 'assetMovement') == true then
-            oX1 = getPropertyFromGroup('opponentStrums', 0, 'x')
-            oX2 = getPropertyFromGroup('opponentStrums', 1, 'x')
-            oX3 = getPropertyFromGroup('opponentStrums', 2, 'x')
-            oX4 = getPropertyFromGroup('opponentStrums', 3, 'x')
-            oY1 = getPropertyFromGroup('opponentStrums', 0, 'y')
-            oY2 = getPropertyFromGroup('opponentStrums', 1, 'y')
-            oY3 = getPropertyFromGroup('opponentStrums', 2, 'y')
-            oY4 = getPropertyFromGroup('opponentStrums', 3, 'y')
-            pX1 = getPropertyFromGroup('playerStrums', 0, 'x')
-            pX2 = getPropertyFromGroup('playerStrums', 1, 'x')
-            pX3 = getPropertyFromGroup('playerStrums', 2, 'x')
-            pX4 = getPropertyFromGroup('playerStrums', 3, 'x')
             pY1 = getPropertyFromGroup('playerStrums', 0, 'y')
             pY2 = getPropertyFromGroup('playerStrums', 1, 'y')
             pY3 = getPropertyFromGroup('playerStrums', 2, 'y')
             pY4 = getPropertyFromGroup('playerStrums', 3, 'y')
             if value1 == 0 then
-                noteTweenY("pY",4,pY1,value2,"quartInOut");
-                noteTweenY("pY1",5,pY2,value2,"quartInOut");
-                noteTweenY("pY2",6,pY3,value2,"quartInOut");
-                noteTweenY("pY3",7,pY4,value2,"quartInOut"); 
+                noteTweenY("pY",4,dpsy0,value2,"quartInOut");
+                noteTweenY("pY1",5,dpsy2,value2,"quartInOut");
+                noteTweenY("pY2",6,dpsy3,value2,"quartInOut");
+                noteTweenY("pY3",7,dpsy4,value2,"quartInOut"); 
             else
                 noteTweenY("pY",4,pY1+value1,value2,"quartInOut");
                 noteTweenY("pY1",5,pY2+value1,value2,"quartInOut");
