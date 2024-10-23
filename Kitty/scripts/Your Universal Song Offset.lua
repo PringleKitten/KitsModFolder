0916254261d46404d2a79ca992695a3e2ad42aaa
PringleKitten <wie054024@gmail.com>
2024-10-22 21:46:00 -0500

    2.35.3 M
    Guns o-o
    HealthBar script fix
    Fixed debug printing and a small error
    Offset script disabled unless you enable it yourself. (true-false)

--- Kitty/scripts/Your Universal Song Offset.lua
+++ Kitty/scripts/Your Universal Song Offset.lua
@@ -1,7 +1,12 @@
-offset = 125
+offset = 150
+local c = false -- set to True to use custom offsets
 
 function onCreate()
+    if not c then
+        offset = getPropertyFromClass('ClientPrefs','noteOffset')
+    elseif c then
     setPropertyFromClass('ClientPrefs','noteOffset',offset) --Number is YOUR Song Offset
+    end
     if songName == 'marshmallow-(alone)' then
        setPropertyFromClass('ClientPrefs','noteOffset',offset-50) --Number is YOUR Song Offset
     end
