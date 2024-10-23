070220f9bec886683f6da822511bd9dcdb48f76e
PringleKitten <wie054024@gmail.com>
2024-10-22 21:44:16 -0500

    2.35.3 E
    Guns o-o
    HealthBar script fix
    Fixed debug printing and a small error
    Offset script disabled unless you enable it yourself. (true-false)

--- mods/Kitty/custom_events/healthBarRotate.lua
+++ mods/Kitty/custom_events/healthBarRotate.lua
@@ -1,19 +1,16 @@
 function onEvent(name, value1, value2)
     if name == 'healthBarRotate' then
+        value1 = tonumber(value1)
+        value2 = tonumber(value2)
         doTweenAngle('rr', 'healthBar', value1, value2, 'circInOut')
         setProperty('iconP1.alpha', 0)
         setProperty('iconP2.alpha', 0)
         if value1 ~= 0 then
-            a = true
-        else
-            a = false
+            setProperty('iconP1.alpha', 0)
+            setProperty('iconP2.alpha', 0)
+        elseif value1 == 0 then
+            setProperty('iconP1.alpha', 1)
+            setProperty('iconP2.alpha', 1)
         end
-    end
-end
-
-function onUpdate()
-    if a then
-        setProperty('iconP1.alpha', 0)
-        setProperty('iconP2.alpha', 0)
     end
 end
