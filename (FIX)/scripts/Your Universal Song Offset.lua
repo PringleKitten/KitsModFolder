716cd7ded6761ff50dbb6feda7304fedae1c1165
PringleKitten <wie054024@gmail.com>
2025-01-15 20:34:54 -0600

    Update 3.8
    Some stuff might break, didn't test it all.

--- (FIX)/scripts/Your Universal Song Offset.lua
+++
@@ -1,8 +0,0 @@
-offset = 75
-
-function onCreate()
-    setPropertyFromClass('ClientPrefs','noteOffset',offset) --Number is YOUR Song Offset
-    if songName == 'mm-alone' then
-       setPropertyFromClass('ClientPrefs','noteOffset',offset-20) --Number is YOUR Song Offset
-    end
-end
