1756d1729bef8e81dbcceaf7590d3b9e2bcbc780
PringleKitten <wie054024@gmail.com>
2025-01-19 03:20:07 -0600

    3.9 Real
    Scoring system fixed / New freeplay saved score look!

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -66,7 +66,7 @@ class FreeplayState extends MusicBeatState
 
 		#if DISCORD_ALLOWED
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
+		DiscordClient.changePresence("In Freeplay Menu", null);
 		#end
 
 		if(WeekData.weeksList.length < 1)
