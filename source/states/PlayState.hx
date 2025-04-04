0ad92d9ae8b3d6dc9e948216287d195f1d5a2adb
PringleKitten <wie054024@gmail.com>
2025-04-03 22:19:59 -0500

    3.12.6
    whoops

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -353,10 +353,10 @@ class PlayState extends MusicBeatState
 		curStage = SONG.stage;
 
 		var stageData:StageFile = StageData.getStageFile(curStage);
-		if (stageData.defaultUIZoom != null)
+
+		defaultCamUIZoom = 1;
+		if (!Math.isNaN(stageData.defaultUIZoom))
 			defaultCamUIZoom = stageData.defaultUIZoom;
-		else
-			defaultCamUIZoom = 1;
 
 		stageUI = "normal";
 		if (stageData.stageUI != null && stageData.stageUI.trim().length > 0)
